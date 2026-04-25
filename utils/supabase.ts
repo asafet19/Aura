d9517f6885c643b20f15238e14f3e5be5d599daa
asafet19 <asafet19@gmail.com>
2026-04-25 12:46:19 +0200

    Security patch: Removed hardcoded Supabase keys for real

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://becsewrnnzhzdqnbjhrs.supabase.co';
-
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlY3Nld3JubnpoemRxbmJqaHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTgzODMsImV4cCI6MjA5MjYzNDM4M30.8lbvd7CTMCgTSh-HcTyvdK2yPImoO65Znv5-IoMvkKE'; 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
